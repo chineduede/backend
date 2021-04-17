56741f6142419b964f043aacedde2210f9b8a18e
chinedu <chndvz@gmail.com>
2021-04-17 07:07:30 +0100

    second-commit

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-const url = process.env.MONGODB_URi
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => {
